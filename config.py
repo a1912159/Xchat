afcdc88eb0c8ca7bbc5b7a197e112b3c3efb6d78
a1912159 <a1912159@adelaide.edu.au>
2024-07-21 21:46:05 +0930

    config.py
    This is the script to configure your server.

---
+++ config.py
@@ -0,0 +1,21 @@
+# Define the domain name for this server
+DOMAIN = "s2"
+
+# Mapping of domain names to their respective WebSocket server URIs
+server_mapping = {
+    "s1": "ws://10.13.83.155:5555",
+    "s2": "ws://10.13.89.245:5555",
+    "s4": "ws://10.13.101.145:5555",
+    "s6": "ws://10.13.69.152:5555",
+    "xchat":"ws://10.13.81.30:5555"
+    }
+
+# Registered users with their hashed passwords
+registered_users = {
+    "lucio@s2": "7549920a8f8c5dec3f1dcdb7a5eb7840ea1e52f2ee40fe70b6d1fb376aed3a8d",
+    "tushar@s2": "7d082e414f09c5ca390197afe48109943e2597c782e58e4262b14303477c1b6b",
+    "abrar@s2": "12b0c9632941cc3aa7075eacc55d4919cadd5c1bd8690460e696bee538db87aa"
+}
+
+
+
